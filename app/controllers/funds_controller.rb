55c137b9379b3c9e7f4ce3dac8a171ffa725fe94
Micah Lee <micah@micahlee.com>
2019-10-02 20:44:34 -0400

    Make funds editable

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -1,6 +1,6 @@
 class FundsController < ApplicationController
   def index
-    @funds = Fund.all
+    @funds = Fund.order(:name).all
   end
 
   def new 
@@ -27,6 +27,20 @@ class FundsController < ApplicationController
  
     @fund.save
     redirect_to @fund
+  end
+
+  def edit
+    @fund = Fund.find(params[:id])
+  end
+
+  def update
+    @fund = Fund.find(params[:id])
+ 
+    if @fund.update(fund_params)
+      redirect_to @fund
+    else
+      render 'edit'
+    end
   end
 
   private
